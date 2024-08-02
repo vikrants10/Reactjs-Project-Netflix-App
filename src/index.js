d6bac453f64a79e2dadc711e36d8bc67dffa94b2
vikrants10 <vikrantkumarsingh333@gmail.com>
2024-08-02 21:07:07 +0530

    REACTJS PROJECT

--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "./Card";
+import "./index.css";
+import sdata from "./sdata";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// console.log(sdata[0].sname);
+
+ReactDOM.render(
+  <>
+    <h1 className="heading_style"> List of top 5 Netflix Series in 2024</h1>
+    {sdata.map((val, index) => {
+      console.log(index);
+      return (
+        <Card
+        key={val.id}
+      imgsrc={val.imgsrc}
+      title={val.title}
+      sname={val.sname}
+      link={val.link}
+    />
+      );
+    })}
+    {/* <Card
+      imgsrc={sdata[0].imgsrc}
+      title={sdata[0].title}
+      sname={sdata[0].sname}
+      link={sdata[0].link}
+    />
+
+    <Card
+      imgsrc={sdata[1].imgsrc}
+      title={sdata[1].title}
+      sname={sdata[1].sname}
+      link={sdata[1].link}
+    />
+
+    <Card
+      imgsrc={sdata[2].imgsrc}
+      title={sdata[2].title}
+      sname={sdata[2].sname}
+      link={sdata[2].link}
+    />
+
+    <Card
+      imgsrc={sdata[3].imgsrc}
+      title={sdata[3].title}
+      sname={sdata[3].sname}
+      link={sdata[3].link}
+    />
+
+    <Card
+      imgsrc={sdata[4].imgsrc}
+      title={sdata[4].title}
+      sname={sdata[4].sname}
+      link={sdata[4].link}
+    /> */}
+  </>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/sdata.jsx
@@ -0,0 +1,39 @@
+const sdata = [
+    {
+        id: 1,
+        sname: '1899',
+        imgsrc:"https://pbs.twimg.com/media/FchXP-zWAAEgYh_.jpg:large", 
+        title:" A Netflix Original Series",
+        link: "https://www.netflix.com/in/title/80990668?source=35",
+    },
+    {
+        id: 2,
+        sname: 'EXTRA CURRICULAR',
+        imgsrc:"https://occ-0-33-37.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABZfOyJelh8gNoR6CGlRnn6Az-2fBifHCfVwBXLPqVh-LkMPBaFk9MU8zDylJFdzBBuMpx-li7QZzq0H1VjzA9DpWZm2TDFdGTrkn9Qz7H78YMYMRn2xb-_JkdIDGVTlBd5DwDg.jpg?r=647", 
+        title:" A Netflix Original Series",
+        link: "https://www.netflix.com/in/title/80990668?source=35",
+    },
+    {
+        id: 3,
+        sname: 'STRANGERS THINGS',
+        imgsrc: "https://wallpapercave.com/wp/wp1917154.jpg", 
+        title: " A Netflix Original Series",
+        link: "https://www.netflix.com/in/title/80990668?source=35",
+    },
+    {
+        id: 4,
+        sname: "RESIDENT EVIL",
+        imgsrc: "https://www.residentevil.com/re4/assets/images/topics/bnr-vrmode-re.jpg",
+        title: "A Netflix Original Series",
+        link: "https://www.netflix.com/in/title/80990668?source=35",
+    },
+    {
+        id: 5,
+        sname: "THE SANDMAN",
+        imgsrc: "https://rukminim2.flixcart.com/image/850/1000/xif0q/poster/g/c/o/large-t0213-the-sandman-netflix-poster-18-x-12-inch-300-gsm-original-imaggsxhggxkfjhw.jpeg?q=90&crop=false",
+        title:" A Netflix Original Series",
+        link: "https://www.netflix.com/in/title/80990668?source=35",
+    },
+];
+
+export default sdata;
